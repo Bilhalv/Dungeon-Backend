d9aa3b306d106fe0f5ec4f83f4e41d03651ebdc2
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-11-28 15:25:48 -0300

    ✨ feat(food): better formatting

--- routes/foods.ts
+++ routes/foods.ts
@@ -1,15 +1,51 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
+import { Request, Response } from "express-serve-static-core";
+import { ParsedQs } from "qs";
 
 const prisma = new PrismaClient({
   log: ["query", "info", "warn", "error"],
 });
 const router = Router();
 
-router.get("/", async (req, res) => {
+async function formatFood(id: string) {
+  try {
+    const food = await prisma.foods.findFirst({ where: { id } });
+
+    if (!food) {
+      return { erro: "Refeição não encontrada" };
+    }
+
+    const carrinhosSoldAmt = await prisma.carrinhos.count({
+      where: { sold: true },
+    });
+    const carrinhosPendingAmt = await prisma.carrinhos.count({
+      where: { sold: false },
+    });
+
+    const monstersOnFood = await prisma.monstersOnFoods.findMany({
+      where: { food_id: food.id },
+    });
+    const monsters = await prisma.monsters.findMany({
+      where: { id: { in: monstersOnFood.map((m) => m.monster_id) } },
+    });
+
+    return {
+      ...food,
+      carrinhosSold: carrinhosSoldAmt,
+      carrinhosPending: carrinhosPendingAmt,
+      monsters,
+    };
+  } catch (error) {
+    return { erro: "Erro ao formatar refei o" };
+  }
+}
+
+router.get("/", async (req: Request, res: Response) => {
   try {
     const foods = await prisma.foods.findMany();
-    res.status(200).json(foods);
+    const results = await Promise.all(foods.map((f) => formatFood(f.id)));
+    res.status(200).json(results);
   } catch (error) {
     res.status(400).json(error);
   }
@@ -17,12 +53,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const foods = await prisma.foods.findMany(
-      {
-        where: { id: req.params.id },
-      }
-    );
-    res.status(200).json(foods);
+    res.status(200).json(await formatFood(req.params.id));
   } catch (error) {
     res.status(400).json(error);
   }
@@ -30,12 +61,13 @@ router.get("/:id", async (req, res) => {
 
 router.get("/search/:name", async (req, res) => {
   try {
-    const foods = await prisma.foods.findMany(
-      {
-        where: { name: { contains: req.params.name, mode: "insensitive" } },
-      }
-    );
-    res.status(200).json(foods);
+    const foods = await prisma.foods.findMany({
+      where: { name: { contains: req.params.name, mode: "insensitive" } },
+    });
+
+    const results = await Promise.all(foods.map((f) => formatFood(f.id)));
+
+    res.status(200).json(results);
   } catch (error) {
     res.status(400).json(error);
   }
